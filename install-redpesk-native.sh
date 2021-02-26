89f7f86202e060ee5493dbf07b045b0b99d3374f
Corentin LE GALL <corentin.legall@iot.bzh>
2021-02-26 11:53:28 +0100

    [native-install] Add license
    Signed-off-by: Corentin LE GALL <corentin.legall@iot.bzh>

--- install-redpesk-native.sh
+++ install-redpesk-native.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+###########################################################################
+# Copyright (C) 2020, 2021 IoT.bzh
+#
+# Authors:   Corentin Le Gall <corentin.legall@iot.bzh>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###########################################################################
 
 SUPPORTED_DISTROS="Ubuntu 20.04, OpenSUSE Leap 15.2, Fedora 33, 32 and 31"
 REDPESK_REPO="https://download.redpesk.bzh/redpesk-devel/releases/33/sdk/"
